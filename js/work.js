db79a06a649703785342dbdccf4311fb37728780
Adam Tengku <adam@bang.com.au>
2015-02-09 06:56:45 +1100

    work isotope

--- js/work.js
+++ js/work.js
@@ -11,12 +11,72 @@
 		
 		if(wrap.find('.iso-wrap')[0]) {
 			initIsotope();
+			initFilters();
 		}
 	})
 	
+	function initFilters() {
+		var elem = wrap.find('.work-filter'),
+			filtBtns = elem.find('a:not(.filter-reset)'),
+			filtReset = elem.find('a.filter-reset'),
+			mobBtn = elem.find('button');
+		mobBtn.click(function() {
+			elem.toggleClass('show');	
+		});
+		// toggle active class
+		filtBtns.click(function(e) {
+			e.preventDefault();
+			var $this = $(this);
+			$this.toggleClass('active');
+			if($this.hasClass('active')) {
+				filtReset.removeClass('active');
+			} else {
+				checkFilters();	
+			}
+			buildFilter();
+		});
+		filtReset.click(function(e) {
+			e.preventDefault();
+			var $this = $(this);
+			if(!$this.hasClass('active')) {
+				$this.addClass('active');
+				filtBtns.removeClass('active');
+			}
+			filterEntries("");
+		});
+		function checkFilters() {
+			var cleared = true;
+			filtBtns.each(function(ind) {
+                var $this = $(this);
+				if($this.hasClass('active')) {
+					cleared = false;
+					return;
+				}
+            });	
+			if(cleared) filtReset.addClass('active');
+		}
+		function buildFilter() {
+			var filtArr = [],
+				filtStr;
+			filtBtns.each(function(ind) {
+                var $this = $(this);
+				if($this.hasClass('active')) {
+					var filrVal = $(this).data('filter-value');
+					filtArr.push(filrVal);
+				}
+            });
+			filtStr = filtArr.join(",");
+			filterEntries(filtStr);
+		}
+		function filterEntries(val) {
+			console.log(val);
+			$('.iso-wrap').isotope({ filter: val });
+		}
+	}
+	
 	function initIsotope() {
 		var isoWrap = wrap.find('.iso-wrap'),
-			isoItem = isoWrap.find('.iso-box');
+			isoItem = isoWrap.find('.item');
 		  
 		isoWrap.isotope({
 			layoutMode: "fitRows",
@@ -26,30 +86,27 @@
 			}
 		});
 		// inview load
-		//initInview();
+		initInview();
 		
 		function initInview() {
 			isoItem.one('inview', function(event, visible) {
-			  var $this = j(this),
-					invi = $this.find('.bg-blue, .ht-info');
+			  var $this = $(this),
+			  	  cont = $this.find('.content'),
+			  	  isLoaded = $this.hasClass('inView');
+			  if(isLoaded) return;
+			 
 			  if (visible) {
 				$this.addClass('inView');
 				if($this.find('img')[0]) {
 					var nTag = $this.find('img'),
 						nSrc = nTag.attr('src');
-				
-					j.imgpreload(nSrc,function() {
-						$this.css({
+					$.imgpreload(nSrc,function() {
+						cont.css({
 							'background-image' : 'url('+nSrc+')'
 						});
-						TweenLite.to($this, 0.6, {css:{'opacity': 1}, ease:Quad.easeOut});
+						TweenLite.to(cont, 0.6, {css:{'opacity': 1}, ease:Quad.easeOut});
+						nTag.remove();
 					});
-				} else {
-					/*invi.css({
-						'opacity' : 0,
-						'visibility' : 'visible'	
-					})*/
-					//TweenLite.to(blrb, 1, {css:{'opacity': 1}, ease:Quad.easeOut});
 				}
 			  }
 			});
